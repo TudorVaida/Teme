8d2c380170d3b2747bf918846a51ae1330102f5e
Tudor Vaida <vaida.tudor@gmail.com>
2022-03-29 10:18:19 +0300

    Cu cumentarii

--- TemaJS-Part2/script.js
+++ TemaJS-Part2/script.js
@@ -1,6 +1,6 @@
 "use strict";
-
 console.log("-------------Exercitiul 1------------");
+/** O functie "getDigits" care primeste un sir de caractere si returneaza cifrele din sirul respectiv*/
 const str = "edr23fef34fds34";
 function getDigits(str) {
 	const arr = [...str];
@@ -14,6 +14,7 @@ function getDigits(str) {
 }
 console.log(getDigits(str));
 console.log("-------------Exercitiul 2------------");
+/** functie "getLetters" care primeste un sir de caractere si returneaza doar literele din sirul respectiv */
 function getLetters(str) {
 	const arr = [...str];
 	let arr2 = [];
@@ -26,6 +27,7 @@ function getLetters(str) {
 }
 console.log(getLetters(str));
 console.log("-------------Exercitiul 3------------");
+/**O functie "getFirst5Letters" care primeste un sir de caractere si returneaza primele 5 litere(daca exista) */
 function getFirst5Letters(str) {
 	const arr = [...str];
 	let arr2 = [];
@@ -38,6 +40,7 @@ function getFirst5Letters(str) {
 }
 console.log(getFirst5Letters(str));
 console.log("-------------Exercitiul 4------------");
+/**O functie "concatenate" care primeste o lista de siruri de caractere si returneaza sirurile concatenate */
 
 const arr2 = ["a", "b", "c", "d"];
 function concatenate(arr) {
@@ -49,6 +52,7 @@ function concatenate(arr) {
 }
 console.log(concatenate(arr2));
 console.log("-------------Exercitiul 5------------");
+/**O functie "getAllDigits" care primeste o lista de siruri de caractere si returneaza cifrele din toate sirurile */
 const arr3 = ["ace", "b22", "c44fdsf", "d323f"];
 function getAllDigits(arr) {
 	let str = "";
@@ -59,11 +63,14 @@ function getAllDigits(arr) {
 }
 console.log(getAllDigits(arr3));
 console.log("-------------Exercitiul 6------------");
+/**O functie "invertAllStrings" care primeste o lista de siruri de caractere si returneaza lista de siruri de caractere inversate
+ */
 function invertAllStrings(str) {
 	return str.map((elem) => [...elem].reverse().join(""));
 }
 console.log(invertAllStrings(arr3));
 console.log("-------------Exercitiul 7------------");
+/**Calculeaza factorialul unui numar ("factorial") */
 function factorial(nr) {
 	if (nr === 0 || nr === 1) {
 		return 1;
@@ -77,6 +84,7 @@ console.log(factorial(0));
 console.log(factorial(3));
 console.log(factorial(5));
 console.log("-------------Exercitiul 8------------");
+/**Calculeaza cel mai mare divizor comun al 2 numere ("cmmdc") */
 function cmmdc(a, b) {
 	while (a != b) {
 		if (a > b) {
@@ -89,6 +97,7 @@ console.log(cmmdc(2, 5));
 console.log(cmmdc(15, 5));
 console.log(cmmdc(124, 34632));
 console.log("-------------Exercitiul 9------------");
+/**Calculeaza cel mai mic multiplu comun al 2 numere ("cmmmc") */
 function cmmmc(a, b) {
 	let m = a;
 	let n = b;
@@ -103,6 +112,7 @@ console.log(cmmmc(2, 5));
 console.log(cmmmc(15, 5));
 console.log(cmmmc(124, 34632));
 console.log("-------------Exercitiul 10------------");
+/**Returneaza un array care sa contina toti divizorii unui numar (ex pentru 64: trebuie sa returneze [2,4,8,16,32]) ("divizori") */
 function divizori(nr) {
 	let arr = [];
 	for (let i = 2; i < nr; i++) {
@@ -116,6 +126,7 @@ console.log(divizori(6));
 console.log(divizori(8));
 console.log(divizori(10));
 console.log("-------------Exercitiul 11------------");
+/**O functie care verifica daca un numar este palindrom (ex: 121, 1234321) ("palindrom") */
 function palindrom(nr) {
 	nr = String(nr);
 	if (nr === [...nr].reverse().join("")) {
@@ -126,6 +137,7 @@ console.log(palindrom(123321));
 console.log(palindrom(123234254));
 console.log(palindrom(567765));
 console.log("-------------Exercitiul 12------------");
+/**O functie care sorteaza numerele pare dintr-un sir de numere primit ca parametru. ("sort") */
 const arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function sort(arr) {
 	let arrPar = [];
@@ -139,6 +151,7 @@ function sort(arr) {
 }
 console.log(sort(arr5));
 console.log("-------------Exercitiul 13------------");
+/**O functie care primeste ca parametru un array de numere. Aceasta sorteaza ascendent numerele pare si descendent numerele impare, in cadrul aceluiasi array primit ca parameru. ("sortAscDesc") */
 function sortAscDesc(arr) {
 	const arrPar = sort(arr);
 	const arrImpar = [];
@@ -153,6 +166,7 @@ function sortAscDesc(arr) {
 }
 console.log(sortAscDesc(arr5));
 console.log("-------------Exercitiul 14------------");
+/**O functie care primeste 2 parametri(un array si un numar). Folosind binary search verificati daca numarul primit ca parametru se gaseste in array. ("binarySearch") */
 const arr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function binarySearch(arr, nr) {
@@ -176,6 +190,7 @@ console.log(binarySearch(arr6, 11));
 console.log(binarySearch(arr6, 8));
 
 console.log("-------------Exercitiul 15------------");
+/**O functie care implementeaza binary search pentru a verifica daca un numar se regaseste intr-un array. Dupa ce se termina executia functiei trebuie sa returnati de cate ori s-a apelat functia recursiv ("countBinarySearch") */
 function countBinarySearch(arr, nr) {
 	let start = 0;
 	let end = arr.length - 1;
