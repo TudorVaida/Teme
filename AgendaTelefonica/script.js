90fabd7be858adafaa5af596a9c81ff7af1cc0bc
Tudor Vaida <vaida.tudor@gmail.com>
2022-04-05 14:27:03 +0300

    VAlidare

--- AgendaTelefonica/script.js
+++ AgendaTelefonica/script.js
@@ -33,11 +33,12 @@ function addContact() {
 	const name = document.querySelector("[name = 'nume']");
 	const nrTel = document.querySelector("[name='nrTel']");
 
-	contacts.list.push({ nume: name.value, nrTel: nrTel.value });
+	checkAdd(name.value, nrTel.value);
 
 	updateUI();
-	// name.value = "";
-	// nrTel.value = "";
+
+	name.value = "";
+	nrTel.value = "";
 }
 function del(idx) {
 	if (confirm("Delete contact?")) {
@@ -45,13 +46,59 @@ function del(idx) {
 		updateUI();
 	}
 }
+function checkAdd(name, nrTel) {
+	const inputs = document.querySelectorAll(".container-input input");
+
+	let arr = [...nrTel];
+	let indicator = true;
+	arr.forEach((el) => {
+		if (isNaN(el)) {
+			indicator = false;
+		}
+	});
+
+	if (indicator && arr.length === 10 && name !== "") {
+		contacts.list.push({ nume: name, nrTel: nrTel });
+
+		inputs.forEach((el) => {
+			el.classList.remove("error");
+		});
+	} else {
+		inputs.forEach((el) => {
+			el.classList.add("error");
+		});
+	}
+}
+function checkSave(name, nrTel, idx) {
+	const inputNume = document.querySelector(`.input-nume${idx}`);
+	const inputNrtel = document.querySelector(`.input-nrTel${idx}`);
+	let arr = [...nrTel];
+	let indicator = true;
+	arr.forEach((el) => {
+		if (isNaN(el)) {
+			indicator = false;
+		}
+	});
+
+	if (indicator && arr.length === 10 && name !== "") {
+		contacts.list[idx] = { nume: name, nrTel: nrTel };
+		inputNume.classList.remove("error");
+		inputNrtel.classList.remove("error");
+		return true;
+	} else {
+		inputNume.classList.add("error");
+		inputNrtel.classList.add("error");
+		return false;
+	}
+}
 function edit(idx) {
-	const elem = contacts.list[idx];
 	const currentContact = document.querySelector(`#contact${idx}`);
 	const currentCredentials = document.querySelector(`.contact${idx}`);
 	let html = `
-    <input class="input-nume${idx}" type="text" value=${contacts.list[idx].nume} name="nume${idx}" /><br>
-    <input class="input-nrTel${idx}" type="text" value=${contacts.list[idx].nrTel} name="nrTel${idx}" /><br>
+    <input class="input-nume${idx}" type="text" value=${contacts.list[idx].nume} name="nume${idx}" />
+    <br>
+    <input class="input-nrTel${idx}" type="text" value=${contacts.list[idx].nrTel} name="nrTel${idx}" />
+    <br>
     <button onclick="save(${idx})">Save</button>`;
 	currentContact.insertAdjacentHTML("afterbegin", html);
 	currentCredentials.classList.add("hidden");
@@ -60,6 +107,7 @@ function edit(idx) {
 function save(idx) {
 	const name = document.querySelector(`.input-nume${idx}`).value;
 	const nrTel = document.querySelector(`.input-nrTel${idx}`).value;
-	contacts.list[idx] = { nume: name, nrTel: nrTel };
-	updateUI();
+	if (checkSave(name, nrTel, idx)) {
+		updateUI();
+	}
 }
